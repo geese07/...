6dba6ac03030676c38468b2a84a899007a23208c
geese_cultists <lobastovdanila@gmail.com>
2024-09-07 19:17:32 +0400

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,10 @@
+def print_params(a: object = 1, b: object = 'str', c: object = True) -> object:
+    print(a, b, c)
+    return a, b, c
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list_2 = [54.32, 'str']
+print_params(*values_list_2, 42)
+values_dict = {'a' : 23, 'b' : "str", 'c' : 12.21}
+print_params(**values_dict)
