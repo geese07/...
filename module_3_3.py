757f3cb4f093be5f685b89497e4f1ff2d47e6c77
geese_cultists <lobastovdanila@gmail.com>
2024-09-08 11:19:37 +0400

    module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -3,6 +3,8 @@ def print_params(a: object = 1, b: object = 'str', c: object = True) -> object:
     return a, b, c
 print_params()
 print_params(b = 25)
+values_list = [2.3, False, "str"]
+print_params(*values_list)
 print_params(c = [1,2,3])
 values_list_2 = [54.32, 'str']
 print_params(*values_list_2, 42)
